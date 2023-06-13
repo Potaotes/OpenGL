d6e4612616b55d6aa0fac4e32a0609033d13e3f3
zhoujiaxiang <zhoujiaxiang@SH-zhougx>
2023-06-13 16:56:34 +0800

    DialyUpdate

--- OpenGLProgram/OpenGL/main.cpp
+++ OpenGLProgram/OpenGL/main.cpp
@@ -17,31 +17,38 @@ void display(GLFWwindow* pWindow, double dCurrentTime)
 
 int main()
 {
+	// 初始化GLFW库
 	if (!glfwInit())
 	{
 		exit(EXIT_FAILURE);
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// 该代码置顶了计算机必须与OpenGL版本 4.3 兼容
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);	// 主版本号
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);	// 此版本号
 
+	// 创建GLFW窗口 参数: 宽, 高, 窗口标题, 允许全屏显示, 允许资源共享
 	GLFWwindow* pWindow = glfwCreateWindow(600, 600, "TestTitle", nullptr, nullptr);
 
+	// GLFW窗口默认双缓冲 GLFW窗口并不会自动将其与OpenGL实例以及状态信息（颜色缓冲区等）相关联。调用此函数关联内容
 	glfwMakeContextCurrent(pWindow);
 
+	// 初始化GLEW库
 	if (glewInit() != GLEW_OK)
 	{
 		exit(EXIT_FAILURE);
 	}
 
+	// 开启垂直同步
 	glfwSwapInterval(1);
 
+	// 自定义初始化函数
 	init(pWindow);
 
-	while (!glfwWindowShouldClose(pWindow))
+	while (!glfwWindowShouldClose(pWindow))	// 判断窗口是否关闭
 	{
 		display(pWindow, glfwGetTime());
-		glfwSwapBuffers(pWindow);
+		glfwSwapBuffers(pWindow);	// 绘制屏幕 交换缓冲区
 		glfwPollEvents();
 	}
 
